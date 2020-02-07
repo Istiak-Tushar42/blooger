9052e06fb614cba2db015534861d7a53a76cdf91
unknown <istiak.tushar.it@gmail.com>
2020-02-07 21:03:41 +0600

    Header complete

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+// STICKY NAV
+$(window).on('scroll', function() {
+    if($(window).scrollTop()) {
+        $('.navbar').addClass('sticky');
+    } else {
+        $('navbar').removeClass('sticky');
+    }
+});
