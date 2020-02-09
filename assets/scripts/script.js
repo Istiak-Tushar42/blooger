2e5fc67e228d82992b66cc98eb28c96beab15d13
unknown <istiak.tushar.it@gmail.com>
2020-02-10 00:26:20 +0600

    Newsletter Completed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,3 +6,13 @@ $(window).on('scroll', function() {
         $('navbar').removeClass('sticky');
     }
 });
+
+// OWL-CAROUSEL
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+})
